beb2f0fa95df10d6a8980be2aeb829359328f836
Prakash Singh <archliveceh@gmail.com>
2022-05-05 18:59:13 +0530

    Fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('./config/config');
 const express = require('express');
 const cors = require('cors');
-const router = require('./router');
+
 const { setupLogging } = require('./utils/logging');
 
 const app = express();
@@ -12,13 +12,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 7011;
-app.use(router);
+const PORT = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
   res.send('Messenge Queue');
 });
 
 app.listen(PORT, () => {
-  console.log(`Messenger service is running on port ${PORT}.`);
+  console.log(`Messenge broker service is running on port ${PORT}.`);
 });
